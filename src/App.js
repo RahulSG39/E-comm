e5f7c3f2094f9523c397536d490be40fbe5899bd
RahulSG <rahulgirish39@gmail.com>
2020-07-22 18:30:30 +0530

    added routing features for first component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
-import "./App.css";
-import HomePage from "./pages/homepage/homepage.component"
+import HomePage from "./pages/homepage/homepage.component";
+import { Switch,  Route} from 'react-router-dom';
+import './App.css';
+
+const HatsPage = (props) =>{
+  console.log(props);
+  return(  
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>);  
+
+  };
+
 
 function App() {
-  return <div className="App">
-    <HomePage />
-  </div>;
+  return (
+  <div>
+    <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage}/>
+    </Switch>
+  </div>
+  );
 }
 
 export default App;
