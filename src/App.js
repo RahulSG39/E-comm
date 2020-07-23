20d1ee7322563060d9679ba74a1b29cf28e6bfcf
RahulSG <rahulgirish39@gmail.com>
2020-07-23 15:39:33 +0530

    updating app with shop preview and item

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
-import { Switch,  Route} from 'react-router-dom';
-import './App.css';
-
-const HatsPage = (props) =>{
-  console.log(props);
-  return(  
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>);  
-
-  };
-
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
-  <div>
-    <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route path='/hats' component={HatsPage}/>
-    </Switch>
-  </div>
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
